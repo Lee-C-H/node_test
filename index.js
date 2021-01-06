09c3ec7d2bb1f5580f5a42d4d9302847fbdbceae
Lee-C-H <brisgi2@naver.com>
2021-01-06 21:16:27 +0900

    secret

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const {User} = require("./models/user")
+const config = require("./config/key")
 
-mongoose.connect('mongodb+srv://brisgi:ckdghks12@myapp.og2ej.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('mongoDB connect...')).catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!!!')
+})
+
+app.post('/register', (req,res) => {
+    //회원가입에 필요한 정보를 Client에서 가져오면 데이터베이스에 insert
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+
+    })
 })
 
 app.listen(port, () => {
